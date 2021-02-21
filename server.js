8e8dcd9c3ce759c331da285e2e7c6fb5f434d425
Fatma Tanrisevdi <fatma@armut.com>
2021-02-21 17:17:17 +0300

    no message

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 app.use(express.static("./dist/frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/frontend/index.html" });
+  res.sendFile("index.html", { root: "dist/frontend/" });
 });
 app.listen(process.env.PORT || 8080);
