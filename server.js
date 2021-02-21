eb1fd14ceb828306988f7414a1cc9238d1c27c36
Fatma Tanrisevdi <fatma@armut.com>
2021-02-21 17:00:26 +0300

    allways return index

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 app.use(express.static("./dist/frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/<name-on-package.json>/" });
+  res.sendFile("index.html");
 });
 app.listen(process.env.PORT || 8080);
