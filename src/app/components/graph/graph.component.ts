10164f6a514c6511461df3041436dd7329bd5936
Fatma Tanrisevdi <fatma@armut.com>
2021-02-21 13:55:27 +0300

    sentiment fix

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -25,7 +25,71 @@ export class GraphComponent implements OnInit, AfterViewInit {
 
   constructor() {}
 
+  prepareDataForGraph(arrayOfResult: Array<any>) {
+    const collectNodes = {}
+
+
+    arrayOfResult.forEach((element, i) => {
+      element['concept_list'].array.forEach((conceptList, k) => {
+        if (collectNodes[conceptList.form]) {
+          collectNodes[conceptList.form]['count']++;
+        } else {
+          collectNodes[conceptList.form] = {
+            count: 1,
+            ids: [],
+          };
+        }
+        const key = Object.keys(collectNodes).length || 0;
+        collectNodes[conceptList.form]['ids'].push('n' + key);
+      });
+      
+      
+    });
+    let i = 0;
+    for (const key in collectNodes) {
+      if (Object.prototype.hasOwnProperty.call(collectNodes, key)) {
+        this.g.nodes.push({
+          id: 'n' + i,
+          label: key,
+          labelThreshold: 0,
+          x: Math.random(),
+          y: Math.random(),
+          size: collectNodes[key]['count'],
+          color:
+            '#' +
+            (
+              Math.floor(Math.random() * 16777215).toString(16) + '000000'
+            ).substr(0, 6),
+        });
+        if (collectNodes['ids'].length > 1) {
+          for (
+            let index = 0;
+            index < collectNodes[key]['ids'].length;
+            index++
+          ) {
+            if (index + 1 != collectNodes[key]['ids'].length) {
+              this.g.edges.push({
+                id: 'e' + i + index,
+                source: collectNodes[key]['ids'][index],
+                target: collectNodes[key]['ids'][index + 1],
+                color: '#ccc',
+              });
+            }
+          }
+        }
+      }
+      i++;
+    }
+
+  }
+
   ngOnInit(): void {
+
+
+
+
+
+
     for (let i = 0; i < this.data.length; i++) {
       for (let k = 0; k < this.data[i]['hashtags'].length; k++) {
         if (this.nodes[this.data[i]['hashtags'][k]['text']]) {
